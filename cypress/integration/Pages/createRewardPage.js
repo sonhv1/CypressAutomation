db79ad168ae7226138d60aa5f90c9bd26b481096
sonhv1 <76928751+sonhv1@users.noreply.github.com>
2021-03-30 17:48:25 +0700

    Test

--- cypress/integration/Pages/createRewardPage.js
+++ cypress/integration/Pages/createRewardPage.js
@@ -1,29 +1,153 @@
 /// <reference types = "Cypress" />
 
+var url = {
+    dashboardUrl: "business_intelligence/overview",
+    reportUrl: "reports",
+    inventoryUrl: "inventories",
+    catalogueUrl: "catalogues",
+    campaignUrl: "campaigns",
+    loyaltyUrl: "loyalty/list"
+}
+
+var originUrl = "https://dashboard.perxtech.io/dashboard/p/"
+
+
 class createRewardPage { 
 
     // locator
+    createNewRewardButton() {
+        return cy.get('button[type=button]')
+    }
 
+    dashboards() {
+        return cy.get('[data-key="business_intelligence"] > a')
+    }
+
+    reports() {
+        return cy.get('[data-key="reports"] > a')
+    }
+
+    rewards() {
+        return cy.get('[data-key="rewards"] > a')
+    }
+
+    inventory() {
+        return cy.get('[data-key="inventories"] > a')
+    }
+
+    catalogues() {
+        return cy.get('[data-key="catalogs"] > a')
+    }
+
+    campaigns() {
+        return cy.get('[data-key="campaigns"] > a')
+    }
+
+    campaignsBeta() {
+        return cy.get('[data-key="new_campaigns"] > a')
+    }
+
+    loyalty() {
+        return cy.get('[data-key="loyalties"] > a')
+    }
+
+    rules() {
+        return cy.get('[data-key="transaction_rules"] > a')
+    }
+
+    merchants() {
+        return cy.get('[data-key="merchants"] > a')
+    }
+
+    layoutContent() {
+        return cy.get('.ant-layout-content')
+    }
+
+    dashboardUrl() {
+        return cy.visit('https://dashboard.perxtech.io/dashboard/p/business_intelligence/overview')
+    }
+
+    reportUrl() {
+        return cy.visit('https://dashboard.perxtech.io/dashboard/p/reports')
+    }
+
+    inventoryUrl() {
+        return cy.visit('https://dashboard.perxtech.io/dashboard/p/inventories')
+    }
+
+    catalogueUrl() {
+        return cy.visit('https://dashboard.perxtech.io/dashboard/p/catalogues')
+    }
+
+    campaignUrl() {
+        return cy.visit('https://dashboard.perxtech.io/dashboard/p/campaigns')
+    }
+
+    loyaltyUrl() {
+        return cy.visit('https://dashboard.perxtech.io/dashboard/p/loyalty/list')
+    }
+
+
+
+
+    //open url function
+    visit(url) {
+        cy.get(url)
+        return this
+    }
    
-
     // check title function
     checkTitle(title) {
         cy.title().should('eq', title)
         return this
     }
 
-
+    // check account can create a new reward
     checkRole() {
-        cy.get('button[type=button]').should('be.visible').should('be.enabled');
+        this.createNewRewardButton().should('be.visible').should('be.enabled');
         return this
     }
 
-
+    // create new reward function
     createReward() {
         const crp = new createRewardPage()
-        const createnewBtn = cy.get('button[type=button]')
-        createnewBtn.click()
+        const createNewBtn = cy.get('button[type=button]')
+        createNewBtn.click()
         return this
+    }
+
+    // checkAccessRole() {
+    //     cy.get
+    // }
+
+    checkAuthorization() {
+        this.dashboards().should('not.exist')
+        this.reports().should('not.exist')
+        this.inventory().should('not.exist')
+        this.catalogues().should('not.exist')
+        this.campaigns().should('not.exist')
+        this.loyalty().should('not.exist')
+        this.rules().should('not.exist')
+        this.merchants().should('not.exist')
+        return this
+    }
+
+    //verify url
+    verifyUrl() {
+    //   this.dashboardUrl()
+    //   this.layoutContent().should('be.visible').contains('403 Forbidden')
+    //   this.reportUrl()
+    //   this.layoutContent().should('be.visible').contains('403 Forbidden')
+    //   this.inventoryUrl()
+    //   this.layoutContent().should('be.visible').contains('403 Forbidden')
+    //   this.catalogueUrl()
+    //   this.layoutContent().should('be.visible').contains('403 Forbidden')
+    for(var value in url) {
+        cy.visit(`${originUrl}${url[value]}`)
+        this.layoutContent().should('be.visible').contains('403 Forbidden')
+    }
+      return this
+
     }
 
 }
--- cypress/integration/Runner/Ex1.spec.js
+++ cypress/integration/Runner/Ex1.spec.js
@@ -1,37 +1,78 @@
 /// <reference types="cypress" />
 
-import dashboardPage from "../Pages/dashboardPage";
+//import dashboardPage from "../Pages/dashboardPage";
 import loginPage from "../Pages/loginPage";
 import createRewardPage from "../Pages/createRewardPage";
 
-describe('Test', () => {
-    Cypress.on('uncaught:exception', (err, runnable) => {
-        return false
-    });
-    it('TestLogin', ()=> {
+Cypress.on('uncaught:exception', (err, runnable) => {
+    return false
+});
+
+
+
+describe('Test authorization of user roles and groups', () => {
+    it('Test permission', ()=> {
         const login = new loginPage()
         login.visit()
         const crp = new createRewardPage()
         login.login('reward_admin@dashboard.com', 'reward_admin');
         crp.checkTitle('Perx | Rewards')
+        crp.checkAuthorization()
+        crp.verifyUrl()
     })
-
-
-    it('testRole', () => {
-        const login = new loginPage()
-        login.visit()
-        const crp = new createRewardPage()
-        login.login('reward_admin@dashboard.com', 'reward_admin')
-        crp.checkRole()
-    })
-
-    it('testCreateNewReward', () => {
-        const login = new loginPage()
-        login.visit()
-        const crp = new createRewardPage()
-        login.login('reward_admin@dashboard.com', 'reward_admin')
-        crp.createReward()
-    })
-  
 })
+
+
+// describe('Test Creating a reward', () => {
+//     it('TestLogin', ()=> {
+//         const login = new loginPage()
+//         login.visit()
+//         const crp = new createRewardPage()
+//         login.login('reward_admin@dashboard.com', 'reward_admin');
+//         crp.checkTitle('Perx | Rewards')
+//     })
+    
+
+//     it('testRole', () => {
+//         const login = new loginPage()
+//         login.visit()
+//         const crp = new createRewardPage()
+//         login.login('reward_admin@dashboard.com', 'reward_admin')
+//         crp.checkRole()
+//     })
+
+//     it('testCreateNewReward', () => {
+//         const login = new loginPage()
+//         login.visit()
+//         const crp = new createRewardPage()
+//         login.login('reward_admin@dashboard.com', 'reward_admin')
+//         crp.createReward()
+//     })
+
+//     it('TestAccessRole', ()=> {
+//         const login = new loginPage()
+//         login.visit()
+//         const crp = new createRewardPage()
+//         login.login('reward_admin@dashboard.com', 'reward_admin');
+//     })
+
+  
+// })
+
+// describe('Test Upload a file in bulk list', () => {
+//     it('TestLogin', ()=> {
+//         const login = new loginPage()
+//         login.visit()
+//         const crp = new createRewardPage()
+//         login.login('reward_admin@dashboard.com', 'reward_admin');
+//         crp.checkTitle('Perx | Rewards')
+//     })
+    
+
+  
+// })
+
+  
+  
+
 
