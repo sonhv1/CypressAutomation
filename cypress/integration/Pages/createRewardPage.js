0be2a05641d56fc714538da1884072a92d79f1fa
sonhv1 <76928751+sonhv1@users.noreply.github.com>
2021-03-31 17:26:19 +0700

    xZX

--- cypress/integration/Pages/createRewardPage.js
+++ cypress/integration/Pages/createRewardPage.js
@@ -9,87 +9,41 @@ var url = {
     loyaltyUrl: "loyalty/list"
 }
 
+// Locator 
 var originUrl = "https://dashboard.perxtech.io/dashboard/p/"
+var nameField = ':nth-child(2) > .ant-form-item-control-wrapper > .ant-form-item-control > .ant-form-item-children > div > .ant-input'
+var nextBtn = '.BottomBar__Content-sc-1dqvwjb-1 > div > .ant-btn-primary'
+var createNewRewardButton = 'button[type=button]'
+var dashboards = '[data-key="business_intelligence"] > a'
+var reports = '[data-key="reports"] > a'
+var rewards = '[data-key="rewards"] > a'
+var inventory = '[data-key="inventories"] > a'
+var catalogues = '[data-key="catalogs"] > a'
+var campaigns = '[data-key="campaigns"] > a'
+var campaignsBeta = '[data-key="new_campaigns"] > a'
+var loyalty = '[data-key="loyalties"] > a'
+var rules = '[data-key="transaction_rules"] > a'
+var merchants = '[data-key="merchants"] > a'
+var layoutContent = '.ant-layout-content'
+var dashboardUrl = 'https://dashboard.perxtech.io/dashboard/p/business_intelligence/overview'
+var reportUrl = 'https://dashboard.perxtech.io/dashboard/p/reports'
+var inventoryUrl = 'https://dashboard.perxtech.io/dashboard/p/inventories'
+var catalogueUrl = 'https://dashboard.perxtech.io/dashboard/p/catalogues'
+var campaignUrl = 'https://dashboard.perxtech.io/dashboard/p/campaigns'
+var loyaltyUrl = 'https://dashboard.perxtech.io/dashboard/p/loyalty/list'
+var startDateField = '[style="margin-bottom: 10px;"] > .ant-col > .ant-form-item-control > .ant-form-item-children > .sc-fzoYkl > .sc-fzomME > [style="display: flex; flex-flow: wrap;"] > .sc-fzqMAW > div > .ant-calendar-picker-input'
+var endDateField = '[style="margin-bottom: 0px;"] > .ant-col > .ant-form-item-control > .ant-form-item-children > .sc-fzoYkl > .sc-fzomME > [style="display: flex; flex-flow: wrap;"] > .sc-fzqMAW > div > .ant-calendar-picker-input'
+var calendarBody = '//tbody[@class="ant-calendar-tbody"]'
+var date = 'ant-calendar-date'
+var selectDate = '[style="margin-bottom: 10px;"] > .ant-col > .ant-form-item-control > .ant-form-item-children > .sc-fzoYkl > .sc-fzomME > [style="display: flex; flex-flow: wrap;"] > .sc-fzqMAW > div > .anticon > svg'
+var calendarClear = '//i[@class="anticon anticon-close-circle ant-calendar-picker-clear"]';
+var cc = '//*[@id="root"]/section/section/main/span/div/div[3]/form/div[1]/div/div/div/div[4]/div[2]/div[1]/div[2]/div/span/div/div[1]/div/span[1]/div/i[2]'
+var launchBtn = 'cy.get("[type="submit"]")'
+var editBtn = '//button[@class="ant-btn ant-btn-primary ant-btn-lg"]'
+
 
 
 class createRewardPage { 
-
-    // locator
-    createNewRewardButton() {
-        return cy.get('button[type=button]')
-    }
-
-    dashboards() {
-        return cy.get('[data-key="business_intelligence"] > a')
-    }
-
-    reports() {
-        return cy.get('[data-key="reports"] > a')
-    }
-
-    rewards() {
-        return cy.get('[data-key="rewards"] > a')
-    }
-
-    inventory() {
-        return cy.get('[data-key="inventories"] > a')
-    }
-
-    catalogues() {
-        return cy.get('[data-key="catalogs"] > a')
-    }
-
-    campaigns() {
-        return cy.get('[data-key="campaigns"] > a')
-    }
-
-    campaignsBeta() {
-        return cy.get('[data-key="new_campaigns"] > a')
-    }
-
-    loyalty() {
-        return cy.get('[data-key="loyalties"] > a')
-    }
-
-    rules() {
-        return cy.get('[data-key="transaction_rules"] > a')
-    }
-
-    merchants() {
-        return cy.get('[data-key="merchants"] > a')
-    }
-
-    layoutContent() {
-        return cy.get('.ant-layout-content')
-    }
-
-    dashboardUrl() {
-        return cy.visit('https://dashboard.perxtech.io/dashboard/p/business_intelligence/overview')
-    }
-
-    reportUrl() {
-        return cy.visit('https://dashboard.perxtech.io/dashboard/p/reports')
-    }
-
-    inventoryUrl() {
-        return cy.visit('https://dashboard.perxtech.io/dashboard/p/inventories')
-    }
-
-    catalogueUrl() {
-        return cy.visit('https://dashboard.perxtech.io/dashboard/p/catalogues')
-    }
-
-    campaignUrl() {
-        return cy.visit('https://dashboard.perxtech.io/dashboard/p/campaigns')
-    }
-
-    loyaltyUrl() {
-        return cy.visit('https://dashboard.perxtech.io/dashboard/p/loyalty/list')
-    }
-
-
-
-
     //open url function
     visit(url) {
         cy.get(url)
@@ -104,7 +58,7 @@ class createRewardPage {
 
     // check account can create a new reward
     checkRole() {
-        this.createNewRewardButton().should('be.visible').should('be.enabled');
+        cy.get(createNewRewardButton).should('be.visible').should('be.enabled')
         return this
     }
 
@@ -121,36 +75,82 @@ class createRewardPage {
     // }
 
     checkAuthorization() {
-        this.dashboards().should('not.exist')
-        this.reports().should('not.exist')
-        this.inventory().should('not.exist')
-        this.catalogues().should('not.exist')
-        this.campaigns().should('not.exist')
-        this.loyalty().should('not.exist')
-        this.rules().should('not.exist')
-        this.merchants().should('not.exist')
+        cy.get(dashboards).should('not.exist')
+        cy.get(reports).should('not.exist')
+        cy.get(inventory).should('not.exist')
+        cy.get(catalogues).should('not.exist')
+        cy.get(campaigns).should('not.exist')
+        cy.get(loyalty).should('not.exist')
+        cy.get(rules).should('not.exist')
+        cy.get(merchants).should('not.exist')
         return this
     }
 
     //verify url
     verifyUrl() {
-    //   this.dashboardUrl()
-    //   this.layoutContent().should('be.visible').contains('403 Forbidden')
-    //   this.reportUrl()
-    //   this.layoutContent().should('be.visible').contains('403 Forbidden')
-    //   this.inventoryUrl()
-    //   this.layoutContent().should('be.visible').contains('403 Forbidden')
-    //   this.catalogueUrl()
-    //   this.layoutContent().should('be.visible').contains('403 Forbidden')
     for(var value in url) {
         cy.visit(`${originUrl}${url[value]}`)
-        this.layoutContent().should('be.visible').contains('403 Forbidden')
+        cy.get(layoutContent).should('be.visible').contains('403 Forbidden')
     }
       return this
 
     }
 
+    // input info of course
+    inputInfo(name) {
+        cy.get(nameField).type(name)
+    }
+
+    // click to Next button
+    clickToNext() {
+        cy.get(nextBtn).click()
+    }
+
+    // check valid period includes start date and end date
+    checkValidPeriod() {
+        cy.get(startDateField).should('be.visible').should('be.enabled')
+        cy.get(endDateField).should('be.visible').should('be.enabled')
+    }
+
+
+    selectValidPeriod() {
+    
+    //    // cy.get(startDateField).clear({force: true})
+    //     // cy.get(endDateField).title('eq', 'March 29, 2021').click()
+    //     //cy.get(endDateField).contains('15').click()
+    //     this.clickToNext()
+        //cy.get(startDateField).click().type(Cypress.moment().format('MMM DD, YYYY'))
+        // cy.get(startDateField).each(randomElement => {
+        //     cy.wrap(randomElement).click({force: true});
+        // });
+
+        // cy.get(endDateField).each(randomElement => {
+        //     cy.wrap(randomElement).click({force: true});
+        // });
+        //cy.get(startDateField).click().should('have.value', Cypress.moment().format('DD MM YYYY'))
+       cy.get(startDateField).click()
+    //    const $el = await cy.find(calendarBody) 
+    //    if($el.length > 0) {
+    //        cy.find("March 17, 2021").click()
+    //    }
+    cy.get('body').then(($body) => {
+        if ($body.find(startDateField).length > 0) {
+            cy.contains('15').click()
+        }
+        })
+
+        cy.get(endDateField).click()
+        cy.get('body').then(($body) => {
+            if ($body.find(startDateField).length > 0) {
+                cy.contains('18').click()
+            }
+        })
+        this.clickToNext()
+        cy.get('span').contains('Launch').click()
+       
+    }
 }
+
 
 
 export default createRewardPage
--- cypress/integration/Pages/loginPage.js
+++ cypress/integration/Pages/loginPage.js
@@ -1,27 +1,17 @@
 /// <reference types = "Cypress" />
 
+
+// Locator
+var email = '#email'
+var password = '#password'
+var loginBtn = '.ant-btn'
+
+
 class loginPage {
-    visit() {
-        cy.visit('https://www.perxtech.io/dashboard')
-    }
-
-    userName() {
-        return cy.get('#email')
-    }
-
-    password() {
-        return cy.get('#password')
-    }
-
-    loginBtn() {
-        return cy.get('.ant-btn')
-    }
-
     login(user, pass) {
-        const login = new loginPage()
-        login.userName().type(user)
-        login.password().type(pass)
-        login.loginBtn().click()
+        cy.get(email).type(user)
+        cy.get(password).type(pass)
+        cy.get(loginBtn).click()
         return this
     }
 }
